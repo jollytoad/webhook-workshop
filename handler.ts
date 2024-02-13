df15846d1442a0af495ac82fa33126f38857ebeb
Mark Gibson <mgibson@adaptavist.com>
2024-02-13 15:13:48 +0000

    Log event, add event type defs. Only deploy if token var is set.

--- handler.ts
+++ handler.ts
@@ -1,11 +1,15 @@
 import { cascade } from "$http_fns/cascade.ts";
 import { ok } from "$http_fns/response/ok.ts";
+import type { WebhookEvents } from "npm:gitlab-event-types";
 
 export default cascade(
-  (req: Request) => {
-    const event = req.headers.get("X-GitLab-Event");
+  async (req: Request) => {
+    const eventName = req.headers.get("X-GitLab-Event");
 
-    console.log("GitLab Event:", event);
+    const eventData = (await req.json()) as WebhookEvents;
+
+    console.log("GitLab Event:", eventName);
+    console.log(JSON.stringify(eventData, null, 2));
 
     return ok();
   },
